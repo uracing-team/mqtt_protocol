6a06295190bb5ee9dfc00197289274f88c1cc188
Ignacio Moore <ignaciomoore@outlook.com>
2022-02-23 12:37:54 -0300

    Testing with other machine

--- publisher.py
+++ publisher.py
@@ -1,7 +1,11 @@
+# paho-mqtt doesn't work for linux
 import paho.mqtt.client as mqtt
 import sys
 
 client = mqtt.Client()
+
+ip = "192.168.158.161"  # Felipe
+localhost = "localhost"
 
 if client.connect("localhost", 1883, 60) != 0:
     print("Could not connect to MQTT Broker!")
