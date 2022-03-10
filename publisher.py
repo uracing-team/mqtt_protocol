b33ad4389e88149c1634dff7314ae03b6aa12533
Ignacio Moore <ignaciomoore@outlook.com>
2022-03-10 12:27:07 -0300

    Added simulation for demo

--- publisher.py
+++ publisher.py
@@ -1,22 +1,50 @@
 # paho-mqtt doesn't work for linux
+from email import message
+from pyexpat.errors import messages
 from time import sleep
 import paho.mqtt.client as mqtt
 import sys
+import csv
 
-client = mqtt.Client()
 
-ip = "191.125.6.109"  # Felipe
-localhost = "localhost"
+if __name__ == "__main__":
+    client = mqtt.Client()
 
-if client.connect("localhost", 1883, 60) != 0:
-    print("Could not connect to MQTT Broker!")
-    sys.exit(-1)
+    # IP Addresses
+    felipe = "191.125.6.109"
+    localhost = "localhost"
+    iggy = "192.168.0.10"
 
-# client.publish("bedroom/temperature", "bedroom_temperature celsius=15", 0)
-# client.publish("uracing/car", "speed km/h=250", 0)
+    if client.connect("localhost", 1883, 60) != 0:
+        print("Could not connect to MQTT Broker!")
+        sys.exit(-1)
 
-for i in range(0, 300, 5):
-    client.publish("uracing/car", f"speed km/h={i}", 0)
-    sleep(0.5)
+    # client.publish("bedroom/temperature", "bedroom_temperature celsius=15", 0)
+    # client.publish("uracing/car", "speed km/h=250", 0)
 
-client.disconnect
+    csv_file = "data/processed_data.csv"
+
+    rows = []
+
+    with open(csv_file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        measurements = next(csv_reader)
+
+        for row in csv_reader:
+            rows.append(row)
+
+    """
+    for i in range(0, 300, 5):
+        client.publish("uracing/car", f"speed km/h={i}", 0)
+        sleep(0.5)
+    """
+
+    for row in rows:
+        print(f"Session Time {row[0]} seconds", end="\r")
+        client.publish("uracing/demo", f"speed km/h={row[3]}", 0)
+        client.publish("uracing/demo", f"gear disc={row[7]}", 0)
+        client.publish("uracing/demo", f"enginerpm rpm={row[8]}", 0)
+        client.publish("uracing/demo", f"enginetemperature celsius={row[9]}", 0)
+        sleep(0.05)
+
+    client.disconnect
